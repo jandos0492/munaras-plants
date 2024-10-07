28b3c6b1412a9355454180af34e7e1080dd33f95
jandos0492 <jandos0492@gmail.com>
2024-10-06 22:48:49 -0400

    added logger for redux in development mode

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,30 @@
+// import { legacy_createStore as createStore, combineReducers, applyMiddleware, compose } from "redux";
+// import thunk from "redux-thunk";
+// import logger from 'redux-logger'; // Статический импорт
+// import sessionReducer from "./session";
+
+// const rootReducer = combineReducers({
+//     session: sessionReducer,
+// });
+
+// let enhancer;
+
+// if (process.env.NODE_ENV === 'production') {
+//     enhancer = applyMiddleware(thunk);
+// } else {
+//     const composeEnhancers =
+//         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//     enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+// }
+
+// const configureStore = (preloadedState) => {
+//     return createStore(rootReducer, preloadedState, enhancer);
+// };
+
+// export default configureStore;
+
 import { legacy_createStore as createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import logger from 'redux-logger'; // Статический импорт
 import sessionReducer from "./session";
 
 const rootReducer = combineReducers({
@@ -12,6 +36,7 @@ let enhancer;
 if (process.env.NODE_ENV === 'production') {
     enhancer = applyMiddleware(thunk);
 } else {
+    const logger = require('redux-logger').default;
     const composeEnhancers =
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     enhancer = composeEnhancers(applyMiddleware(thunk, logger));
