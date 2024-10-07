722e1dfe9065dd6878814d36b593fab79c2fecb0
jandos0492 <jandos0492@gmail.com>
2024-10-06 23:23:40 -0400

    fixed the config/database.js file

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,4 @@
-const config = require("./index");
+const config = require('./index');
 
 const db = config.db;
 const username = db.username;
@@ -12,21 +12,21 @@ module.exports = {
     password,
     database,
     host,
-    dialect: "postgres",
-    seedersStorage: "sequelize",
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
   },
   production: {
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
-    seederStorage: "sequelize",
-    dealectOptions: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
-      define: {
-        schema: process.env.SCHEMA
-      }
     },
+    define: {
+      schema: process.env.SCHEMA
+    }
   },
 };
